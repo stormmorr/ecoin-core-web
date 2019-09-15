77dd0a1f0863dbc736df3ee50995401e52a5505f
stormmorr <osirem@ymail.com>
2019-09-15 17:05:25 +0100

    Uploaded Miner Identified
    Used with wallet.js for finity selected of infinity customers

--- javascript/miner-identified.js
+++ javascript/miner-identified.js
@@ -8,7 +8,6 @@ var ecn_walletadr_address = [];
 var ecn_walletadr_masterkey = [];
 
 var username = "osirem";
-var g_IP = "192.168.0.11";
 
 function hextoBase64(f_Hex)
 {
@@ -22,11 +21,6 @@ function hextoBase64(f_Hex)
 	return base64string;
 }
 
-function result_error(err)
-{
-	console.log("ECN-Error result to wallet error: " + JSON.stringify(err));
-}
-
 function makeBaseAuth(user, pswd)
 { 
 	var token = user + ':' + pswd;
@@ -38,181 +32,104 @@ function makeBaseAuth(user, pswd)
 	return "Basic " + hash;
 }
 
-var auth = makeBaseAuth('root','osiPROP37');
-var url = 'http://www.bitcoin-office.com/tracking/login.php?username=root&password=osiPROP37';
-var url2 = 'http://www.bitcoin-office.com/tracking/result.php?username=root&password=osiPROP37';
-
-$.ajax({
-	url : url,
-	method: 'GET',
-	xhrFields: { withCredentials: true },
-	dataType: 'text',
-	//beforeSend : function(req)
-	//	{
-    //    req.setRequestHeader('Authorization', 'Basic cm9vdDpvc2lQUk9QMzc=');
-	//	},
-    crossDomain: true,
-    success: login_success,
-	error: login_success
-	});
-
-function login_success(err)
-{
-	console.log("ECN-logged in with Credentials:(err):" + JSON.stringify(err));
+var ecn_cookiehash = getCookie("ecn_cookiehash");
+var ecn_adrcnt = getCookie("ecn_adrcnt");
 	
-	$.ajax({
-		url : url2,
-		method: 'GET',
-		xhrFields: { withCredentials: true },
-		dataType: 'text',
-		//beforeSend : function(req)
-		//	{
-		//	req.setRequestHeader('Authorization', 'Basic cm9vdDpvc2lQUk9QMzc=');
-		//	},
-		crossDomain: true,
-		success: result_success,
-		error: result_error
-		});
-}
-
-function login_error(err)
-{
-	console.log("ECN-error continuing with Credentials:(err):" + JSON.stringify(err));
+if(ecn_cookiehash == "")
+	{
+	var f_InputToHash = "";
+	var hash = sha256.create();
 	
-	$.ajax({
-		url : url2,
-		method: 'GET',
-		xhrFields: { withCredentials: true },
-		dataType: 'text',
-		//beforeSend : function(req)
-		//	{
-		//	req.setRequestHeader('Authorization', 'Basic cm9vdDpvc2lQUk9QMzc=');
-		//	},
-		crossDomain: true,
-		success: result_success,
-		error: result_error
-		});
-}
+	f_InputToHash += username;
+	f_InputToHash += getip;
+	
+	hash.update(f_InputToHash);
+	var f_hash = hash.hex();
+	
+	ecn_cookiehash = hextoBase64(f_hash);
+	
+	setCookie("ecn_cookiehash", ecn_cookiehash, 2500);
+	}
+	
+g_Wallet.m_hdid = ecn_cookiehash;
 
-function result_success(data, status)
-{
-	var ecn_cookiehash = getCookie("ecn_cookiehash");
-	var ecn_adrcnt = getCookie("ecn_adrcnt");
-		
-	if(ecn_cookiehash == "")
+if(ecn_adrcnt == "")
+	{
+	var f_adrmain = g_Wallet.newAddress(1);
+	
+	setCookie("ecn_walletadr_prefix1", "1", 2500);
+	setCookie("ecn_walletadr_address1", f_adrmain, 2500);
+	setCookie("ecn_walletadr_masterkey1", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
+	
+	var f_adrchange1 = g_Wallet.newAddress(2);
+	
+	setCookie("ecn_walletadr_prefix2", "2", 2500);
+	setCookie("ecn_walletadr_address2", f_adrchange1, 2500);
+	setCookie("ecn_walletadr_masterkey2", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
+	
+	var f_adrchange2 = g_Wallet.newAddress(2);
+	
+	setCookie("ecn_walletadr_prefix3", "2", 2500);
+	setCookie("ecn_walletadr_address3", f_adrchange2, 2500);
+	setCookie("ecn_walletadr_masterkey3", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
+	
+	var f_adrchange3 = g_Wallet.newAddress(2);
+	
+	setCookie("ecn_walletadr_prefix4", "2", 2500);
+	setCookie("ecn_walletadr_address4", f_adrchange3, 2500);
+	setCookie("ecn_walletadr_masterkey4", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
+	
+	var f_adrchange4 = g_Wallet.newAddress(2);
+	
+	setCookie("ecn_walletadr_prefix5", "2", 2500);
+	setCookie("ecn_walletadr_address5", f_adrchange4, 2500);
+	setCookie("ecn_walletadr_masterkey5", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
+
+	ecn_adrcnt = 5;
+	setCookie("ecn_adrcnt", ecn_adrcnt, 2500);
+	}
+else
+	{
+	if(ecn_adrcnt >= 5)
 		{
-		var f_InputToHash = "";
-		var hash = sha256.create();
-		
-		f_InputToHash += username;
-		f_InputToHash += g_IP;
-		
-		hash.update(f_InputToHash);
-		var f_hash = hash.hex();
-		
-		ecn_cookiehash = hextoBase64(f_hash);
-		
-		setCookie("ecn_cookiehash", ecn_cookiehash, 2500);
-		}
-		
-	g_Wallet.m_hdid = ecn_cookiehash;
-
-	if(ecn_adrcnt == "")
-		{
-		var f_adrmain = g_Wallet.newAddress(1);
-		
-		setCookie("ecn_walletadr_prefix1", "1", 2500);
-		setCookie("ecn_walletadr_address1", f_adrmain, 2500);
-		setCookie("ecn_walletadr_masterkey1", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
-		
-		var f_adrchange1 = g_Wallet.newAddress(2);
-		
-		setCookie("ecn_walletadr_prefix2", "2", 2500);
-		setCookie("ecn_walletadr_address2", f_adrchange1, 2500);
-		setCookie("ecn_walletadr_masterkey2", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
-		
-		var f_adrchange2 = g_Wallet.newAddress(2);
-		
-		setCookie("ecn_walletadr_prefix3", "2", 2500);
-		setCookie("ecn_walletadr_address3", f_adrchange2, 2500);
-		setCookie("ecn_walletadr_masterkey3", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
-		
-		var f_adrchange3 = g_Wallet.newAddress(2);
-		
-		setCookie("ecn_walletadr_prefix4", "2", 2500);
-		setCookie("ecn_walletadr_address4", f_adrchange3, 2500);
-		setCookie("ecn_walletadr_masterkey4", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
-		
-		var f_adrchange4 = g_Wallet.newAddress(2);
-		
-		setCookie("ecn_walletadr_prefix5", "2", 2500);
-		setCookie("ecn_walletadr_address5", f_adrchange4, 2500);
-		setCookie("ecn_walletadr_masterkey5", g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
-
-		ecn_adrcnt = 5;
-		setCookie("ecn_adrcnt", ecn_adrcnt, 2500);
+		for(var f_i = 1; f_i <= ecn_adrcnt; f_i++)
+			{
+			ecn_walletadr_prefix[f_i - 1] = getCookie("ecn_walletadr_prefix" + f_i);
+			ecn_walletadr_address[f_i - 1] = getCookie("ecn_walletadr_address" + f_i);
+			ecn_walletadr_masterkey[f_i - 1] = getCookie("ecn_walletadr_masterkey" + f_i);
+			const f_Key = bitcoin.ECPair.fromWIF(ecn_walletadr_masterkey[f_i - 1]);
+			
+			g_Wallet.addKey(f_Key, ecn_walletadr_address[f_i - 1], ecn_walletadr_prefix[f_i - 1]);
+			console.log("masterkey: " + ecn_walletadr_masterkey[f_i - 1]);
+			console.log("address: " + ecn_walletadr_address[f_i - 1]);
+			}
 		}
 	else
 		{
-		if(ecn_adrcnt >= 5)
+		for(var f_i = 1; f_i <= ecn_adrcnt; f_i++)
 			{
-			for(var f_i = 1; f_i <= ecn_adrcnt; f_i++)
-				{
-				ecn_walletadr_prefix = getCookie("ecn_walletadr_prefix" + f_i);
-				ecn_walletadr_address = getCookie("ecn_walletadr_address" + f_i);
-				ecn_walletadr_masterkey = getCookie("ecn_walletadr_masterkey" + f_i);
-				const f_Key = bitcoin.ECPair.fromWIF(ecn_walletadr_masterkey);
-				
-				g_Wallet.addKey(f_Key, ecn_walletadr_address, ecn_walletadr_prefix);
-				console.log("masterkey: " + ecn_walletadr_masterkey);
-				console.log("address: " + ecn_walletadr_address);
-				}
+			ecn_walletadr_prefix[f_i - 1] = getCookie("ecn_walletadr_prefix" + f_i);
+			ecn_walletadr_address[f_i - 1] = getCookie("ecn_walletadr_address" + f_i);
+			ecn_walletadr_masterkey[f_i - 1] = getCookie("ecn_walletadr_masterkey" + f_i);
+			
+			const f_Key = bitcoin.ECPair.fromWIF(ecn_walletadr_masterkey[f_i - 1]);
+			
+			g_Wallet.addKey(f_Key, ecn_walletadr_address[f_i - 1], ecn_walletadr_prefix[f_i - 1]);
 			}
-		else
+			
+		for(var f_trl = ecn_adrcnt; f_trl <= 5; f_trl++)
 			{
-			for(var f_i = 1; f_i <= ecn_adrcnt; f_i++)
-				{
-				ecn_walletadr_prefix = getCookie("ecn_walletadr_prefix" + f_i);
-				ecn_walletadr_address = getCookie("ecn_walletadr_address" + f_i);
-				ecn_walletadr_masterkey = getCookie("ecn_walletadr_masterkey" + f_i);
-				
-				const f_Key = bitcoin.ECPair.fromWIF(ecn_walletadr_masterkey);
-				
-				g_Wallet.addKey(f_Key, ecn_walletadr_address, ecn_walletadr_prefix);
-				}
-				
-			for(var f_trl = ecn_adrcnt; f_trl <= 5; f_trl++)
-				{
-				var f_adrchange = g_Wallet.newAddress(2);
-		
-				setCookie("ecn_walletadr_prefix" + f_trl, "1", 2500);
-				setCookie("ecn_walletadr_address" + f_trl, f_adrchange, 2500);
-				setCookie("ecn_walletadr_masterkey" + f_trl, g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
-				}
-				
-			ecn_adrcnt = 5;
-			setCookie("ecn_adrcnt", ecn_adrcnt, 2500);
+			var f_adrchange = g_Wallet.newAddress(2);
+	
+			setCookie("ecn_walletadr_prefix" + f_trl, "1", 2500);
+			setCookie("ecn_walletadr_address" + f_trl, f_adrchange, 2500);
+			setCookie("ecn_walletadr_masterkey" + f_trl, g_Wallet.m_vec_Key[g_Wallet.m_idx_vec_Key - 1].toWIF(), 2500);
 			}
+			
+		ecn_adrcnt = 5;
+		setCookie("ecn_adrcnt", ecn_adrcnt, 2500);
 		}
-		
-	$.ajax({
-		url : url,
-		method: 'GET',
-		xhrFields: { withCredentials: true },
-		dataType: 'text',
-		//beforeSend : function(req)
-		//	{
-		//    req.setRequestHeader('Authorization', 'Basic cm9vdDpvc2lQUk9QMzc=');
-		//	},
-		crossDomain: true,
-		success: function(err)
-			{
-			g_Wallet.Update();
-			console.log("ECN-save keys success");
-			},
-		error: function(err)
-			{
-			console.log("ECN-Failed to save keys");
-			},
-		});	
-}
+	}
+	
+g_Wallet.Update();
+console.log("WALLET ADDRESS = " + g_Wallet.GetAdr());
