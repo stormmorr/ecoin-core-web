1d08d59f1cbed14580b9106c7a5427453903c190
stormmorr <osirem@ymail.com>
2019-11-05 07:48:21 +0000

    Add files via upload

---
+++ credentials.class.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Web miner Javascript V1.0<br />
+
+developed by LuckHubbie .
+June 2016 :
+below you can configure the credentials sa mining pool.
+Any pool can be used , but it is advised to use a p2pool to a remote or local mining.
+*/
+//manual method
+   $osiurl[0] = "p2pool.org";//"bitcoin-office.com";//the pool link you want to use . for example: eu.p2pool.cloud
+   $osiport[0] = "9332";//This is the port to use. see: https://pt.wikipedia.org/wiki/Proxy or :http://lkhubbie.tk/protocols/
+   $osiusername[0] = "1CoqPPh5zMS8YJNtH31B9wbAKyG1EpXvao";//"3CjoQ1HgkpvZDH3sH5igfEhcq9A1MWrz6U";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//"bitcoinrpc";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//use your bitcoin address.
+   $osipassword[0] = "x";//"dfbufbSUBUSbf7763YSFYbfybsiyb87";//can be anything from this space is not empty.
+   $osiurl[1] = "p2pool.org";//"bitcoin-office.com";//the pool link you want to use . for example: eu.p2pool.cloud
+   $osiport[1] = "9332";//This is the port to use. see: https://pt.wikipedia.org/wiki/Proxy or :http://lkhubbie.tk/protocols/
+   $osiusername[1] = "1CoqPPh5zMS8YJNtH31B9wbAKyG1EpXvao";//"osirem7.worker1"//"stormmorr.001";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//"3CjoQ1HgkpvZDH3sH5igfEhcq9A1MWrz6U";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//"bitcoinrpc";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//use your bitcoin address.
+   $osipassword[1] = "x";//"dfbufbSUBUSbf7763YSFYbfybsiyb87";//can be anything from this space is not empty.
+   $osiurl[2] = "bitcoin-office.com";//the pool link you want to use . for example: eu.p2pool.cloud
+   $osiport[2] = "10332";//This is the port to use. see: https://pt.wikipedia.org/wiki/Proxy or :http://lkhubbie.tk/protocols/
+   $osiusername[2] = "stormmorr.5";//"3CjoQ1HgkpvZDH3sH5igfEhcq9A1MWrz6U";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//"bitcoinrpc";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//use your bitcoin address.
+   $osipassword[2] = "5";//,d=64"dfbufbSUBUSbf7763YSFYbfybsiyb87";//can be anything from this space is not empty.
+   $osiurl[3] = "bitcoin-office.com";//the pool link you want to use . for example: eu.p2pool.cloud
+   $osiport[3] = "9332";//This is the port to use. see: https://pt.wikipedia.org/wiki/Proxy or :http://lkhubbie.tk/protocols/
+   $osiusername[3] = "admin.1";//"3CjoQ1HgkpvZDH3sH5igfEhcq9A1MWrz6U";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//"bitcoinrpc";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//use your bitcoin address.
+   $osipassword[3] = "1";//,d=64"dfbufbSUBUSbf7763YSFYbfybsiyb87";//can be anything from this space is not empty.
+   $osiurl[4] = "bitcoin-office.com";//the pool link you want to use . for example: eu.p2pool.cloud
+   $osiport[4] = "9332";//This is the port to use. see: https://pt.wikipedia.org/wiki/Proxy or :http://lkhubbie.tk/protocols/
+   $osiusername[4] = "admin.1";//"3CjoQ1HgkpvZDH3sH5igfEhcq9A1MWrz6U";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//"bitcoinrpc";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//use your bitcoin address.
+   $osipassword[4] = "1";//,d=64"dfbufbSUBUSbf7763YSFYbfybsiyb87";//can be anything from this space is not empty.
+   $osiurl[5] = "bitcoin-office.com";//the pool link you want to use . for example: eu.p2pool.cloud
+   $osiport[5] = "8181";//This is the port to use. see: https://pt.wikipedia.org/wiki/Proxy or :http://lkhubbie.tk/protocols/
+   $osiusername[5] = "x";//"3CjoQ1HgkpvZDH3sH5igfEhcq9A1MWrz6U";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//"bitcoinrpc";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//use your bitcoin address.
+   $osipassword[5] = "x";//,d=64"dfbufbSUBUSbf7763YSFYbfybsiyb87";//can be anything from this space is not empty.
+   $osiurl[6] = "bitcoin-office.com";//the pool link you want to use . for example: eu.p2pool.cloud
+   $osiport[6] = "8181";//This is the port to use. see: https://pt.wikipedia.org/wiki/Proxy or :http://lkhubbie.tk/protocols/
+   $osiusername[6] = "x";//"3CjoQ1HgkpvZDH3sH5igfEhcq9A1MWrz6U";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//"bitcoinrpc";//"1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";//use your bitcoin address.
+   $osipassword[6] = "x";//,d=64"dfbufbSUBUSbf7763YSFYbfybsiyb87";//can be anything from this space is not empty.
+/*
+if you want to automatically mine for a pool that the system identifies as faster or viable mining. edit these parameters below
+
+    $response = json_decode(file_get_contents("http://nodes.p2pool.co/pools.dat"),true);
+    $url = $response[6]['name'];
+    $port = $response[6]['port'];
+    $username = "1Dyf1KQvazHoRrFXDCPxesNRbvKtkveT7o";
+    $password = "x";
+*/
+?>
